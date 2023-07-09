ce8d9cf38d441ec2df6be0da51da0c9e8efc7a7e
Vestanra <35749970+Vestanra@users.noreply.github.com>
2023-07-09 22:14:41 +0300

    01

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,35 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body')
+
+btnStart.addEventListener('click', onBtnStart);
+btnStop.addEventListener('click', onBtnStop);
+
+btnStop.disabled = true;
+let timerId = null;
+
+function onBtnStart() {
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+    changeColor();
+};
+
+function onBtnStop() {
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+    clearInterval(timerId);
+};
+
+function changeColor() {
+    timerId = setInterval(() => {
+        bodyEl.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+
+function stopChangeColor() {
+    setInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
